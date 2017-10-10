4b595096db3af58107e76879d2a27898606d3a98
AlexUnderMoscow <guns-19@yandex.ru>
2017-10-10 18:46:46 +0400

    pretty quicksort

--- quicksort.c
+++ quicksort.c
@@ -1,40 +1,41 @@
 
-//pivot
+//в целях отладки размер статического масссива для его просмотра
 #define cnt 7
+//статический массив
 static int tmp[cnt];
+//деление подмассива на части с меньшими и большими элементами
+//отностительно центрального, который избирается последним в списке
+//выталкивание бОльших элементов вперед подобно пузырьковой сортировке
 int partition (int arr[], int low, int high)
 {
-    int pivot = arr[high];    // pivot
-    int i = low ;  // Index of smaller element (low - 1)
-
+    int pivot = arr[high];    // избираем центральный элемент
+    int i = low ;  //индекс минимального - low
+    //цикл продвижения больших элементов вперед
     for (int j = low; j <= high- 1; j++)
     {
-        // If current element is smaller than or
-        // equal to pivot
+       //если элемент менее центрального, то минимальный
+        //элемент меняется местами с текущим
         if (arr[j] <= pivot)
         {
             swap(&arr[i], &arr[j]); memcpy(tmp,arr,cnt*sizeof(int));
-            i++;    // increment index of smaller element
+            i++;
         }
     }
+    //обмен максимального элемента с последним
     swap(&arr[i], &arr[high]); memcpy(tmp,arr,cnt*sizeof(int));
+    //возврат центрального элемента
     return i;
 }
 
-/* The main function that implements QuickSort
- arr[] --> Array to be sorted,
-  low  --> Starting index,
-  high  --> Ending index */
 void AlexQuickSort(int arr[], int low, int high)
 {
     memcpy(tmp,arr,cnt*sizeof(int));
     if (low < high)
     {
-        /* pi is partitioning index, arr[p] is now
-           at right place */
+        //определение центрального элемента, виталкивание больших
+        //вперед и сортировка
         int pi = partition(arr, low, high);memcpy(tmp,arr,cnt*sizeof(int));
-        // Separately sort elements before
-        // partition and after partition
+        //рекурсивный вызов сортировки для подмассивов левого и правого
         AlexQuickSort(arr, low, pi - 1);
         AlexQuickSort(arr, pi + 1, high);
     }
